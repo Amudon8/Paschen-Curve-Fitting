fa62223dba395f59474aa36b1a04a4d55d6617f0
Amudon <124877121+Amudon8@users.noreply.github.com>
2023-10-30 22:54:10 +0530

    Add files via upload

--- fixed distance.py
+++ fixed distance.py
@@ -3,51 +3,44 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
 
-def func(x, b, c):
-    return x*b/(c + np.log(x))
-
-
 # experimental data
 pres = [9.7e-2, 1.2e-1, 1.4e-1, 1.6e-1, 1.8e-1, 2.0e-1, 2.2e-1, 2.4e-1, 2.8e-1, 3.5e-1, 4.1e-1]
 dist = 10
 pd = np.multiply(dist, pres)
 volt = [428, 354.5, 356.75, 327.75, 336.5, 339.25, 341.25, 334.25, 357, 393.75, 425.5]
 
-# finding optimised B and C
-popt, poct = curve_fit(func, pd, volt)
 
-# plotting the curve fitting
-b_opt, c_opt = popt
+def func(x, b, a, c):
+    return b*x/(np.log(a*x)-np.log(np.log(1 + 1.0/c)))
+
+
+# finding optimised B and C
+popt, poct = curve_fit(func, pd, volt, p0=[300,20,0.02])
+print(popt)
+# the fitted curve
 pd_model = np.linspace(min(pd), max(pd), 100)
-volt_model = func(pd_model, b_opt, c_opt)
+volt_model = func(pd_model, popt[0], popt[1], popt[2])
 
 # finding pd_min and v_min from the optimised function graph
 v_min = min(volt_model)
 volt_model_l = volt_model.tolist()
 index = volt_model_l.index(v_min)
-
 pd_min = pd_model[index]
 
-# the constant b given by v_min/pd_min
-b = v_min/(pd_min)
-c = 1 - np.log(pd_min)
-volt_graph = func(pd_model, b, c)
+b_graph = v_min/pd_min
+print(b_graph)
+a_graph = b_graph/15.76
+print(a_graph)
+gamma = 1/(np.exp(a_graph*pd_min/2.72)-1)
+print(gamma)
 
-u_argon = 15.77
-print('The ionization energy of argon is ', u_argon, ' eV')
-print('The value of B is\f', b_opt)
-
-a_opt = b_opt/u_argon
-print('The value of A is', a_opt)
-
-gamma_opt = 1/(np.exp(a_opt/np.exp(c_opt))-1)
-print('The value of gamma is ', gamma_opt)
-
-plt.plot(pd_model, volt_model, color='r')
-plt.plot(pd_model, volt_graph, color='green')
+# standard deviation
+y_error =[9.34, 6.45, 7.59, 3.20, 2.38, 0.5, 2.06, 2.63, 3.16, 0.96, 2.64]
+plt.plot(pd_model, volt_model, color='red')
 plt.scatter(pd, volt, s=5)
-plt.title('Paschen Curve of fixed distance')
+plt.errorbar(pd, volt, yerr=y_error, fmt='.')
+plt.title('Paschen Curve of fixed Distance')
 plt.xlabel('pd (mbar.cm)')
 plt.ylabel('Breakdown Voltage(volts)')
-plt.legend(['Obtimised parameter','Graph parameter', 'Data'])
+plt.legend(['Fitted Curve', 'Data'])
 plt.show()
--- fixed pressure.py
+++ fixed pressure.py
@@ -3,50 +3,42 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
 
-def func(x, b, c):
-    return x*b/(c + np.log(x))
+def func(x, b, a, c):
+    return b*x/(np.log(a*x)-np.log(np.log(1 + 1.0/c)))
 
 # experimental data
 pres = 1.6e-1
-dist = [9, 8, 7, 6, 5, 4, 3]
+dist = [12, 11, 10, 9, 8, 7, 6, 5, 4, 3]
 pd = np.multiply(dist, pres)
-volt = [294.5, 283.75, 275, 275.5, 281.25, 316.75, 435]
+volt = [350.5, 334.25, 310.25, 294.5, 283.75, 275, 275.5, 281.25, 316.75, 434]
 
 # finding optimised B and C
-popt, poct = curve_fit(func, pd, volt)
-
-# plotting the curve fitting
-b_opt, c_opt = popt
+popt, poct = curve_fit(func, pd, volt, p0=[300,20,0.02])
+print(popt)
+# the fitted curve
 pd_model = np.linspace(min(pd), max(pd), 100)
-volt_model = func(pd_model, b_opt, c_opt)
+volt_model = func(pd_model, popt[0], popt[1], popt[2])
 
 # finding pd_min and v_min from the optimised function graph
 v_min = min(volt_model)
 volt_model_l = volt_model.tolist()
 index = volt_model_l.index(v_min)
-
 pd_min = pd_model[index]
 
-# the constant b given by v_min/pd_min
-b = v_min/(pd_min)
-c = 1 - np.log(pd_min)
-volt_graph = func(pd_model, b, c)
+b_graph = v_min/pd_min
+print(b_graph)
+a_graph = b_graph/15.76
+print(a_graph)
+gamma = 1/(np.exp(a_graph*pd_min/2.72)-1)
+print(gamma)
 
-u_argon = 15.77
-print('The ionization energy of argon is ', u_argon, ' eV')
-print('The value of B is\f', b_opt)
-
-a_opt = b_opt/u_argon
-print('The value of A is', a_opt)
-
-gamma_opt = 1/(np.exp(a_opt/np.exp(c_opt))-1)
-print('The value of gamma is ', gamma_opt)
-
-plt.plot(pd_model, volt_model, color='r')
-plt.plot(pd_model, volt_graph, color='green')
+# standard deviation
+y_error = [1.91, 2.22, 2.63, 3.70, 2.06, 2.94, 0.58, 1.26, 0.5, 3.61]
+plt.plot(pd_model, volt_model, color='red')
 plt.scatter(pd, volt, s=5)
+plt.errorbar(pd, volt, yerr=y_error, fmt='.')
 plt.title('Paschen Curve of fixed pressure')
 plt.xlabel('pd (mbar.cm)')
 plt.ylabel('Breakdown Voltage(volts)')
-plt.legend(['Obtimised parameter','Graph parameter', 'Data'])
+plt.legend(['Fitted Curve', 'Data'])
 plt.show()
